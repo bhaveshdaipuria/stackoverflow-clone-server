722e1c80c7cacc288b126105bde0de883c4cae74
Bhavesh Daipuria <bdaipuria@gmail.com>
2025-08-25 17:27:29 +0530

    feat: changed ai model from openai to gemini

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -1,91 +1,102 @@
-import OpenAI from "openai";
 import nodemailer from "nodemailer";
 import OTPSchema from "../models/Otp.js";
+import { GoogleGenAI } from "@google/genai";
 
 function expireOTP(setUser) {
-  setTimeout(async () => {
-    await OTPSchema.deleteOne(setUser);
-  }, 300000);
+	setTimeout(async () => {
+		await OTPSchema.deleteOne(setUser);
+	}, 300000);
 }
 
 export const generateOTP = async (req, res) => {
-  const { clientMail, user } = req.body;
+	const { clientMail, user } = req.body;
 
-  try {
-    const generatedOTP = await OTPSchema.findOne({ user });
-    const otp = Math.floor(1000 + Math.random() * 9000);
-    const transport = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_ID,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-    const mailOptions = {
-      from: process.env.EMAIL_ID,
-      to: clientMail,
-      subject: "Your OTP for chatbot",
-      text: `Your generated OTP is ${otp}. It will expire in 5 minutes`,
-    };
-    transport.sendMail(mailOptions, function (error, response) {
-      if (error) {
-        res.json({ error: error });
-        console.log("Error");
-      } else {
-        res.json({ success: "Email has been sent successfully" });
-      }
-    });
-    if (generatedOTP) {
-      await OTPSchema.findOneAndReplace({ user }, { user: user, otp: otp });
-      expireOTP({ user: user });
-    } else {
-      await OTPSchema.create({
-        user: user,
-        otp: otp,
-      });
-      expireOTP({ user: user });
-    }
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const generatedOTP = await OTPSchema.findOne({ user });
+		const otp = Math.floor(1000 + Math.random() * 9000);
+		const transport = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				user: process.env.EMAIL_ID,
+				pass: process.env.EMAIL_PASS,
+			},
+		});
+		const mailOptions = {
+			from: process.env.EMAIL_ID,
+			to: clientMail,
+			subject: "Your OTP for chatbot",
+			text: `Your generated OTP is ${otp}. It will expire in 5 minutes`,
+		};
+		transport.sendMail(mailOptions, function(error, response) {
+			if (error) {
+				res.json({ error: error });
+				console.log("Error");
+			} else {
+				res.json({ success: "Email has been sent successfully" });
+			}
+		});
+		if (generatedOTP) {
+			await OTPSchema.findOneAndReplace({ user }, { user: user, otp: otp });
+			expireOTP({ user: user });
+		} else {
+			await OTPSchema.create({
+				user: user,
+				otp: otp,
+			});
+			expireOTP({ user: user });
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const verifyOTP = async (req, res) => {
-  const { enteredOTP, user } = req.body;
-  try {
-    const currentUser = await OTPSchema.findOne({ user });
-    if (currentUser) {
-      if (enteredOTP == currentUser.otp) {
-        res.json({ success: currentUser.otp });
-        await OTPSchema.deleteOne({ user });
-      } else {
-        res.json({ failure: "Wrong OTP entered" });
-      }
-    } else {
-      res.json({ failure: "Please login first" });
-    }
-  } catch (error) {
-    console.error(error);
-  }
+	const { enteredOTP, user } = req.body;
+	try {
+		const currentUser = await OTPSchema.findOne({ user });
+		if (currentUser) {
+			if (enteredOTP == currentUser.otp) {
+				res.json({ success: currentUser.otp });
+				await OTPSchema.deleteOne({ user });
+			} else {
+				res.json({ failure: "Wrong OTP entered" });
+			}
+		} else {
+			res.json({ failure: "Please login first" });
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const askChatbot = async (req, res) => {
-  const { chatbotQues } = req.body;
+	const { chatbotQues } = req.body;
 
-  const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_KEY,
-  });
+	const geminiBot = new GoogleGenAI({
+		apiKey: process.env.GEMINI_API_KEY,
+	});
 
-  try {
-    const response = await openai.chat.completions.create({
-      messages: [{ role: "user", content: chatbotQues }],
-      model: "gpt-3.5-turbo",
-    });
-    res.json({ message: response.data.choices[0].text });
-  } catch (error) {
-    console.error(error.type);
-    if (error.type === "insufficient_quota") {
-      res.status(429).json({ insufficient_quota: true });
-    }
-  }
+	try {
+
+		const response = await geminiBot.models.generateContent({
+			model: process.env.GEMINI_MODEL,
+			contents: [
+				{
+					role: "user",
+					parts: [
+						{ text: chatbotQues }
+					]
+				}
+			]
+		});
+
+		const parts = response.candidates?.[0]?.content?.parts || [];
+		answer = parts.map(p => p.text).join(" ") || "No response";
+		res.status(200).json({ message: answer });
+	} catch (error) {
+		console.error(error.type);
+		if (error.type === "insufficient_quota") {
+			res.status(429).json({ insufficient_quota: true });
+		}
+	}
 };
